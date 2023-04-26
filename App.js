74b8d648c53ea10676660a12f17a7e05592427bc
Brooklyn Dippo <brooklyndippo@gmail.com>
2023-04-25 21:48:33 -1000

    add stack navigation, create separate screens for dogs and cats, add buttons to navigate between

--- App.js
+++ App.js
@@ -1,21 +1,71 @@
 import { StatusBar } from 'expo-status-bar';
-import { FlatList, SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native';
-import { dogs } from './breeds';
+import { Button, FlatList, SafeAreaView, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { cats, dogs } from './breeds';
 import Item from './Item';
+
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>By Breeds!</Text>
+      <TouchableOpacity
+        onPress={() => navigation.navigate('Dogs')}
+      >
+        <Text style={styles.button}>Dog Breeds 🐕</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => navigation.navigate('Cats')}
+      >
+        <Text style={styles.button}>🐈 Cat Breeds</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+function DogsScreen() {
+  return (
+    <View style={styles.container}>
+    <SafeAreaView>
+      <FlatList 
+        data={dogs}
+        renderItem={({ item, index }) => <Item  breedIndex={index} breedInfo={item}/>}
+        keyExtractor={item => item.breed}
+        contentContainerStyle={styles.listContainer}
+      />
+    </SafeAreaView>
+    <StatusBar style="auto" />
+  </View>
+  )
+}
+
+function CatsScreen() {
+  return (
+    <View style={styles.container}>
+    <SafeAreaView>
+      <FlatList 
+        data={cats}
+        renderItem={({ item, index }) => <Item  breedIndex={index} breedInfo={item}/>}
+        keyExtractor={item => item.breed}
+        contentContainerStyle={styles.listContainer}
+      />
+    </SafeAreaView>
+    <StatusBar style="auto" />
+  </View>
+  )
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <SafeAreaView>
-        <FlatList 
-          data={dogs}
-          renderItem={({ item, index }) => <Item  breedIndex={index} breedInfo={item}/>}
-          keyExtractor={item => item.breed}
-          contentContainerStyle={styles.listContainer}
-        />
-      </SafeAreaView>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Dogs" component={DogsScreen} />
+      <Stack.Screen name="Cats" component={CatsScreen} />
+    </Stack.Navigator>
+  </NavigationContainer>
   );
 }
 
@@ -28,5 +78,13 @@ const styles = StyleSheet.create({
     flexGrow: 1,
     justifyContent: 'space-between',
     padding: 20,
+  },
+  button: {
+    backgroundColor: 'lightblue',
+    padding: 20,
+    borderRadius: 20,
+    margin: 10,
+    width: 200,
+    textAlign: 'center',
   },
 });
